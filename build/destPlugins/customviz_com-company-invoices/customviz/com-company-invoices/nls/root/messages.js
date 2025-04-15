63a52da35bc32b055b64fbf31c563f49e290b536
lhernandezguevara <lhernandezguevara@deloitte.com>
2025-04-15 15:15:26 -0600

    invoices

---
+++ build/destPlugins/customviz_com-company-invoices/customviz/com-company-invoices/nls/root/messages.js
@@ -0,0 +1,9 @@
+define({
+    "INVOICES_DISPLAY_NAME": "Invoices Plugin",
+    "INVOICES_SHORT_DISPLAY_NAME": "Invoices Plugin",
+    "INVOICES_CATEGORY":"Invoices Plugin",
+    "INVOICES_ROW_LABEL":"Invoices Plugin Row",
+    "INVOICES_ITEM_SIZE":"Item Size",
+    "TEXT_MESSAGE": "Hello!  This is the {0} visualization and I have {1} rows of data."
+
+});
